8369127958b61229f4420de93cd4e1a36f5d781c
Andres Medina <andres.medina18@outlook.com>
2023-04-23 17:08:53 -0500

    Primer commit

---
+++ src/main/java/com/cibertec/demo/SaludoCibertecApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaludoCibertecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaludoCibertecApplication.class, args);
+	}
+
+}
